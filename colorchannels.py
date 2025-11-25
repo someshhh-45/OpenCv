8a8b0d3c08ef859e992d56af968ee5369bd0fd69
Somesh <msomeshwar45@gmail.com>
2025-11-26 00:49:23 +0530

    Add files via upload

---
+++ colorchannels.py
@@ -0,0 +1,25 @@
+import cv2 as cv
+import numpy as np 
+img=cv.imread('Resources/Photos/park.jpg')
+cv.imshow('Park',img)
+
+b,g,r=cv.split(img)
+blank =np.zeros(img.shape[:2],dtype='uint8')
+blue=cv.merge([b,blank,blank])
+green=cv.merge([blank,g,blank])
+red=cv.merge([blank,blank,r])
+
+cv.imshow('Blue Channel',blue)
+cv.imshow('Green Channel',green)
+cv.imshow('Red Channel',red)
+
+
+print(img.shape)
+print(blue.shape)          
+print(green.shape)
+print(red.shape)
+
+# merged=cv.merge([b,g,r])
+# cv.imshow('Merged Image',merged)
+
+cv.waitKey(0)
